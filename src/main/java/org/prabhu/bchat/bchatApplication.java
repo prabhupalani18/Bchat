90e18a434ec4e3ca25b217428db398046ad9553a
Prabhu Palani <55530600+prabhupalani18@users.noreply.github.com>
2025-08-09 16:27:22 +0530

    Bchat app

---
+++ src/main/java/org/prabhu/bchat/bchatApplication.java
@@ -0,0 +1,14 @@
+package org.prabhu.bchat;
+
+import org.prabhu.bchat.config.WebhookProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BchatApplication.class, args);
+    }
+
+}
